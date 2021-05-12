6ca2599a5cf24e203f6b56ba8feb5a89c8231963
HackElite <78653024+hackelite01@users.noreply.github.com>
2021-05-12 10:04:57 +0530

    Update __help.py

--- userbot/plugins/__help.py
+++ userbot/plugins/__help.py
@@ -1,11 +1,11 @@
-# Copyright (C) 2020 TeamDerUntergang.
+# Copyright (C) 2021 HackElite
 #
 # SedenUserBot is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# SedenUserBot is distributed in the hope that it will be useful,
+# MarcusUserBot is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
