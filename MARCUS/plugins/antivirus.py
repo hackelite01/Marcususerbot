57e2ddf4cff7e72516f025218c4b3fad3673b010
HackElite <78653024+hackelite01@users.noreply.github.com>
2021-06-20 21:00:32 +0530

    Update antivirus.py

--- MARCUS/plugins/antivirus.py
+++ MARCUS/plugins/antivirus.py
@@ -3,7 +3,7 @@ import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from userbot.utils import admin_cmd,register
+from ..utils import admin_cmd,register
 
 @borg.on(admin_cmd("scan ?(.*)"))
 async def _(event):
